bdc06bc1e3f271e2b6c6b3bf0966e9ece182a4b3
pemrouz <p3pedram@hotmail.com>
2015-07-14 02:25:15 +0100

    update builds

--- dist/client.js
+++ dist/client.js
@@ -1,55 +1,46 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
-
-/* istanbul ignore next */
-var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
-
-// -------------------------------------------
-// Exposes a convenient global instance
-// -------------------------------------------
-module.exports = singleton;
-
-function singleton(ripple) {
-  log("creating");
-  owner.ripple = ripple;
-  return ripple;
-}
-
-var owner = _interopRequire(require("utilise/owner"));
-
-var log = _interopRequire(require("utilise/log"));
-
-log = log("[ri/singleton]");
-},{"utilise/log":2,"utilise/owner":8}],2:[function(require,module,exports){
 module.exports = require('log')
-},{"log":3}],3:[function(require,module,exports){
+},{"log":2}],2:[function(require,module,exports){
 var is = require('is')
   , to = require('to')
+  , owner = require('owner')
 
 module.exports = function log(prefix){
   return function(d){
+    if (!owner.console || !console.log.apply) return d;
     is.arr(arguments[2]) && (arguments[2] = arguments[2].length)
     var args = to.arr(arguments)
-    args.unshift(''.grey ? prefix.grey : prefix)
+    args.unshift(prefix.grey ? prefix.grey : prefix)
     return console.log.apply(console, args), d
   }
 }
-},{"is":4,"to":5}],4:[function(require,module,exports){
-module.exports = { 
-  fn     : isFunction
-, str    : isString
-, num    : isNumber
-, obj    : isObject
-, truthy : isTruthy
-, falsy  : isFalsy
-, arr    : isArray
-, null   : isNull
-, def    : isDef
-, in     : isIn
+},{"is":3,"owner":4,"to":6}],3:[function(require,module,exports){
+module.exports = is
+is.fn     = isFunction
+is.str    = isString
+is.num    = isNumber
+is.obj    = isObject
+is.lit    = isLiteral
+is.bol    = isBoolean
+is.truthy = isTruthy
+is.falsy  = isFalsy
+is.arr    = isArray
+is.null   = isNull
+is.def    = isDef
+is.in     = isIn
+
+function is(v){
+  return function(d){
+    return d == v
+  }
 }
 
 function isFunction(d) {
   return typeof d == 'function'
+}
+
+function isBoolean(d) {
+  return typeof d == 'boolean'
 }
 
 function isString(d) {
@@ -62,6 +53,11 @@ function isNumber(d) {
 
 function isObject(d) {
   return typeof d == 'object'
+}
+
+function isLiteral(d) {
+  return typeof d == 'object' 
+      && !(d instanceof Array)
 }
 
 function isTruthy(d) {
@@ -86,10 +82,18 @@ function isDef(d) {
 
 function isIn(set) {
   return function(d){
-    return ~set.indexOf(d)
+    return  set.indexOf 
+         ? ~set.indexOf(d)
+         :  d in set
   }
 }
-},{}],5:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
+(function (global){
+module.exports = require('client') ? /* istanbul ignore next */ window : global
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"client":5}],5:[function(require,module,exports){
+module.exports = typeof window != 'undefined'
+},{}],6:[function(require,module,exports){
 module.exports = { 
   arr : toArray
 }
@@ -97,12 +101,32 @@ module.exports = {
 function toArray(d){
   return Array.prototype.slice.call(d, 0)
 }
-},{}],6:[function(require,module,exports){
-(function (global){
-module.exports = require('client') ? window : global
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"client":7}],7:[function(require,module,exports){
-module.exports = typeof window != 'undefined'
-},{}],8:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
+arguments[4][4][0].apply(exports,arguments)
+},{"client":8,"dup":4}],8:[function(require,module,exports){
+arguments[4][5][0].apply(exports,arguments)
+},{"dup":5}],9:[function(require,module,exports){
 module.exports = require('owner')
-},{"owner":6}]},{},[1]);
+},{"owner":7}],10:[function(require,module,exports){
+"use strict";
+
+/* istanbul ignore next */
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+// -------------------------------------------
+// Exposes a convenient global instance
+// -------------------------------------------
+module.exports = singleton;
+
+function singleton(ripple) {
+  log("creating");
+  owner.ripple = ripple;
+  return ripple;
+}
+
+var owner = _interopRequire(require("utilise/owner"));
+
+var log = _interopRequire(require("utilise/log"));
+
+log = log("[ri/singleton]");
+},{"utilise/log":1,"utilise/owner":9}]},{},[10]);
