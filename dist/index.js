b9455d551bb6e7c38e4ad35c4963cd77adfc1147
pemrouz <p3pedram@hotmail.com>
2017-07-30 02:58:39 +0100

    wip

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,13 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = singleton;
-
-var _owner = require('utilise/owner');
-
-var _owner2 = _interopRequireDefault(_owner);
-
-/* istanbul ignore next */
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 // -------------------------------------------
 // Exposes a convenient global instance
 // -------------------------------------------
-function singleton(ripple) {
+module.exports = function singleton(ripple) {
   log('creating');
-  if (!_owner2.default.ripple) _owner2.default.ripple = ripple;
+  if (!owner.ripple) owner.ripple = ripple;
   return ripple;
-}
+};
 
-var log = require('utilise/log')('[ri/singleton]');
+var owner = require('utilise/owner'),
+    log = require('utilise/log')('[ri/singleton]');
